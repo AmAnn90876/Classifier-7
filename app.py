5601cc5407a5c7e1f2b1c4356c33b4371d4492f7
AmAnn90876 <amani123456789asd@gmail.com>
2026-07-13 20:32:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -89,6 +89,7 @@ def get_complaints():
         'confidence': f"{c.confidence * 100:.1f}%"
     } for c in complaints]
     return jsonify(data)
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    # إضافة هذا السطر يجعل السيرفر يعمل بشكل صحيح على بيئات الاستضافة
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
